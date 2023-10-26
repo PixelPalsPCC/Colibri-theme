c1675ddad6206c0e6cc9a5ff3d07fda1b723d417
GeorgeMcK <george@hookstream.com>
2023-10-26 11:04:35 -0700

    added basic.php, html.php

---
+++ html.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Introduction</title>
+</head>
+<body>
+    <h1>PHP Introduction</h1>
+    <p><?php echo "Welcome to PHP Introduction!";?></p>
+</body>
+</html>
